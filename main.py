7277894c93f401eef18286b77d74765d36bc27a9
rjhoppe-W <48058894+rjhoppe@users.noreply.github.com>
2023-05-26 13:09:27 -0400

    fix!(func): fixed broken os desktop path and cleaned up function logic Closed some broken function logic. Added print statements through the program to test.

--- main.py
+++ main.py
@@ -6,22 +6,26 @@ def find_files (pdfs=None):
     if pdfs is None:
         pdfs = []
 
-    directory = os.path('C:\\Users\\Rick\\Desktop')
+    directory = (os.environ['USERPROFILE'] + '\Desktop')
     files = os.listdir(directory)
+    # print(files)
     for f in files:
         if f.endswith('.pdf'):
             pdfs.append(f)
+    return pdfs
 
 pdfs = find_files()
+print(pdfs)
 
 def return_files (pdfs):
     print('These are your PDF files available to merge:')
     for f in pdfs:
         print(f)
 
-return_files(pdfs)
+return_files (pdfs)
 
 def select_files(pdfs, pdfs_selected=None):
+    print('Test')
     if pdfs_selected is None:
         pdfs_selected = []
 
@@ -29,32 +33,32 @@ def select_files(pdfs, pdfs_selected=None):
         print('Please select at least two files to merge')
         print("File #1:")
         merge_file1 = input()
-        if merge_file1 in pdfs == False:
-            print("This file does not exist. Please make sure \
-                the file name is spelled correctly and it is \
-                located on your desktop.")
-            return(select_file_one)
+        if merge_file1 in pdfs:
+            pdfs_selected.append(select_file_one)
         elif merge_file1 == 'Exit':
             sys.exit()
         else:
-            pdfs_selected.append(merge_file1)
+            print("This file does not exist. Please make sure the file name is spelled correctly and it is located on your desktop.")
+            return select_file_one ()
+    
+    select_file_one ()
     
     def select_file_two ():
         print("File #2:")
         merge_file2 = input()
-        if merge_file2 in pdfs == False:
-            print("This file does not exist. Please make sure \
-                the file name is spelled correctly and it is \
-                located on your desktop.")
-            return(select_file_two)
-        elif merge_file2 == 'Exit':
-            sys.exit()
-        else:
+        if merge_file2 in pdfs:
             pdfs_selected.append(select_file_one)
             print("Do you have any more files to merge? Y/N")
             answer = input()
             if answer == 'Y':
-                return select_file_two
+                return select_file_two ()
+        elif merge_file2 == 'Exit':
+            sys.exit()
+        else:
+            print("This file does not exist. Please make sure the file name is spelled correctly and it is located on your desktop.")
+            return select_file_two ()
+            
+    select_file_two ()
             
     def merge_files (pdfs_selected):
         merger = PdfMerger()
@@ -65,3 +69,8 @@ def select_files(pdfs, pdfs_selected=None):
         merger.write("mergedfile.pdf")
         merger.close()
         print('Job done.')
+
+    merge_files (pdfs_selected)
+
+select_files(pdfs)
+
