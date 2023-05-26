eb7b45482c6fd6699948a070abb1bfc010df9b88
rjhoppe-W <48058894+rjhoppe@users.noreply.github.com>
2023-05-26 14:45:58 -0400

    fix(func): fix broken logic for Y/N selections in the program Fixed broken logic where function was defaulting to "Y" / "y" value in if / else statements. Working on adding logic to copy targeted files from the desktop.

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def find_files (pdfs=None):
 
     directory = (os.environ['USERPROFILE'] + '\Desktop')
     files = os.listdir(directory)
-    # print(files)
+    print(files)
     for f in files:
         if f.endswith('.pdf'):
             pdfs.append(f)
@@ -34,7 +34,7 @@ def select_files(pdfs, pdfs_selected=None):
         print("File #1:")
         merge_file1 = input()
         if merge_file1 in pdfs:
-            pdfs_selected.append(select_file_one)
+            pdfs_selected.append(merge_file1)
         elif merge_file1 == 'Exit':
             sys.exit()
         else:
@@ -47,31 +47,68 @@ def select_files(pdfs, pdfs_selected=None):
         print(f"File #{func_iterator}:")
         merge_file2 = input()
         if merge_file2 in pdfs:
-            pdfs_selected.append(select_file_one)
+            pdfs_selected.append(merge_file2)
             print("Do you have any more files to merge? Y/N")
             answer = input()
-            if answer == 'Y' or 'y':
+            if answer == 'Y':
+                print(f'Files selected: {pdfs_selected}')
                 func_iterator+=1
                 return select_file_two (func_iterator)
-        elif merge_file2 == 'Exit' or 'exit':
+            if answer == 'N':
+                print(f'Files selected: {pdfs_selected}')
+            else:
+                print("Command not recognized.")
+        elif merge_file2 == 'Exit':
             sys.exit()
-        elif merge_file2 == 'N' or 'n':
-            pass
         else:
             print("This file does not exist. Please make sure the file name is spelled correctly and it is located on your desktop.")
-            return select_file_two ()
+            return select_file_two (func_iterator)
             
     select_file_two ()
             
     def merge_files (pdfs_selected):
-        merger = PdfMerger()
+        print(f'Prepping the following files for merging: {pdfs_selected}')
+        print("Finalize this merge? Y/N")
+        finalize = input()
+        if finalize == 'Y':
+            merger = PdfMerger()
 
-        for p in pdfs_selected:
-            merger.append(p)
+            for p in pdfs_selected:
+                # p = os.environ['USERPROFILE'] + '\Desktop' + p
+                # Need to copy files to this directory
+                merger.append(p)
 
-        merger.write("mergedfile.pdf")
-        merger.close()
-        print('Job done.')
+            merger.write("mergedfile.pdf")
+            merger.close()
+            print('Job done.')
+        
+        elif finalize == 'N':
+            print('What action would you like to take: Add, Delete, Exit')
+            action = input()
+            if action == 'Add': 
+                return select_file_two(func_iterator=(len(pdfs_selected)+1))
+
+            elif action == 'Delete':
+                print('Which file would you like to delete from your list?')
+                print(pdfs_selected)
+                delete_choice = input()
+                if delete_choice in pdfs_selected:
+                    pdfs_selected.remove(delete_choice)
+                    return merge_files (pdfs_selected)
+
+            elif action == 'Exit':
+                sys.exit()
+
+            else:
+                print('Command not recognized, please try again.')
+                return merge_files()
+
+        elif finalize == 'Exit':
+            sys.exit()
+
+        else:
+            print('Command not recognized, please try again.')
+            return merge_files()
 
     merge_files (pdfs_selected)
 
